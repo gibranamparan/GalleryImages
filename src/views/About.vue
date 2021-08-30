dd2384e1e5c88f6c2f86a7a711a050a93506cfff
Gibran Amparán <gamparan@carepointrx.com>
2021-08-29 20:52:02 -0500

    about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is just a task for the interview.</h1>
+    <b>Git Repo </b><a href="https://github.com/gibranamparan/GalleryImages">Here</a>
   </div>
 </template>
 <script lang="ts">
@@ -11,6 +12,6 @@ export default class About extends Vue{
   created(){
     console.log('Created About Page');
   }
-  
+
 }
 </script>
